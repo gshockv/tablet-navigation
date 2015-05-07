0d51abd9b03051c252e1b01c2c484e1a625f7011
Vitaly Gashock <gshockv@gmail.com>
2015-05-07 14:29:06 +0300

    side menu layout

--- app/src/main/java/com/gshockv/tabletnavigation/ApplicationActivity.java
+++ app/src/main/java/com/gshockv/tabletnavigation/ApplicationActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity {
+public class ApplicationActivity extends AppCompatActivity {
     private Toolbar toolbar;
 
     @Override
